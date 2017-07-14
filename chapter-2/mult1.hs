0250be983276abaf6f95fe241f90ef6df9820cb9
Dany Marcoux <danymarcoux@gmail.com>
2017-07-14 12:16:55 +0200

    Finish chapter 2

---
+++ chapter-2/mult1.hs
@@ -0,0 +1,16 @@
+mult1 = x * y
+  where x = 5
+        y = 6
+
+mult2 = x * 3 + y
+  where x = 3
+        y = 1000
+
+mult3 = x * 5
+  where x = 10 * 5 + y
+        y = 10
+
+mult4 = z / x + y
+  where x = 7
+        y = negate x
+        z = y * 10
